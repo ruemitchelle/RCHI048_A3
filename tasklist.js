dbde83204ca7601cf06beddcb895dd345d5c4ff8
ruemitchelle <101538433+ruemitchelle@users.noreply.github.com>
2022-06-03 21:09:30 +1000

    Kanban
    added kanban board, need to finish styling and create ability to add more todos
    
    removed calendar and replaced with assignments for kanban usage

--- tasklist.js
+++ tasklist.js
@@ -1,40 +1,44 @@
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
-const todoDate = document.querySelector('todo-duedate');
 
-todoButton.addEventListener('click', addTodo);
+const taskInput = document.querySelector('.task-input');
+const taskButton = document.querySelector('.task-button');
+const taskList = document.querySelector('.task-list');
 
-function addTodo(event){
+taskButton.addEventListener('click', addTask);
+taskList.addEventListener('click'. deleteCheck)
+
+function addTask(event){
     //prevents from submitting button
     event.preventDefault();
-    //Create todo div
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add("todo");
+    //Create task div
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add("task");
     //Create list items 
-    const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-    // Create dude date 
-    const dueDate = document.createElement('li');
-    const todoDate = todoDate.value;
-    newTodo.innerHTML = todoInput.value;
-    todoDiv.appendChild(dueDate);
+    const newTask = document.createElement('li');
+    newTask.innerText = taskInput.value;
+    newTask.classList.add('task-item');
+    taskDiv.appendChild(newTask);
     //Create completed button
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    completedButton.innerHTML = `<i class="fa-regular fa-circle-check"></i>`;
     completedButton.classList.add("completed-btn");
-    todoDiv.appendChild(completedButton);
+    taskDiv.appendChild(completedButton);
     //Create trash button
     const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    trashButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
     trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
+    taskDiv.appendChild(trashButton);
     //append to list
-    todoList.appendChild(todoDiv);
-    //clear todo input value
-    todoInput.value="";
+    taskList.appendChild(taskDiv);
+    //clear task input value
+    taskInput.value="";
 }
 
-//https://www.youtube.com/watch?v=Ttf3CEsEwMQ&list=PLZHpUd9MSdiaYjwmSq0xxKsYbDrQeRYup&index=5&t=131s referenced for todolist
+function deleteCheck(e) {
+    const item = e.target;
+    //remove task
+
+    if (item.classList[0] === 'trash-btn') {
+        const task = item.parentElement;
+        taskButton.remove()
+    }
+}
