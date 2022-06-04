9bb5e82b7f374ac44e652bf624a981cff8d1afe2
ruemitchelle <101538433+ruemitchelle@users.noreply.github.com>
2022-06-04 16:25:42 +1000

    Dashboard
    completed dashboard and created task manager with date and prio

--- tasklist.js
+++ tasklist.js
@@ -1,44 +1,178 @@
+onload = taskMain;
 
-const taskInput = document.querySelector('.task-input');
-const taskButton = document.querySelector('.task-button');
-const taskList = document.querySelector('.task-list');
+function taskMain(){
+  let inputElem,
+      inputElem2,
+      dueDate,
+      dueTime,
+      button,
+      selectElem;
+      taskList = [];
 
-taskButton.addEventListener('click', addTask);
-taskList.addEventListener('click'. deleteCheck)
+  getElements();
+  addListeners();
+  load();
+  renderRows();
 
-function addTask(event){
-    //prevents from submitting button
-    event.preventDefault();
-    //Create task div
-    const taskDiv = document.createElement('div');
-    taskDiv.classList.add("task");
-    //Create list items 
-    const newTask = document.createElement('li');
-    newTask.innerText = taskInput.value;
-    newTask.classList.add('task-item');
-    taskDiv.appendChild(newTask);
-    //Create completed button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = `<i class="fa-regular fa-circle-check"></i>`;
-    completedButton.classList.add("completed-btn");
-    taskDiv.appendChild(completedButton);
-    //Create trash button
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    trashButton.classList.add("trash-btn");
-    taskDiv.appendChild(trashButton);
-    //append to list
-    taskList.appendChild(taskDiv);
-    //clear task input value
-    taskInput.value="";
+  function getElements(){
+    inputElem = document.getElementsByTagName("input")[0];
+    inputElem2 = document.getElementsByTagName("input")[1];
+    dateInput = document.getElementById('dueDate');
+    timeInput = document.getElementById('dueTime');
+    button = document.getElementById('addBtn');
+    selectElem = document.getElementById('priorityFilter');
+  }
+
+  function addListeners(){
+    button.addEventListener("click", addEntry, false);
+    selectElem.addEventListener('change', filterEntries, false);
+  }
+
+  function addEntry(event){
+
+    let inputValue = inputElem.value;
+    inputElem.value = "";
+
+    let inputValue2 = inputElem2.value;
+    inputElem.value = "";
+
+    let dateValue = dateInput.value;
+    dateInput.value = "";
+
+    let timeValue = timeInput.value;
+    timeInput.value = "";
+
+    let obj = {
+        task : inputValue,
+        category: inputValue2,
+        date: dateValue,
+        time: timeValue,
+        done: false,
+    }
+
+    renderRow(obj);
+
+    taskList.push(inputValue);
+
+    save();
+    
+
+  }
+
+  function filterEntries() {
+
+    let selection = selectElem.value;
+
+    if(selection == "all"){
+        let rows = document.getElementsByTagName('tr');
+
+      Array.from(rows).forEach((row, index)=>{
+     
+        row.style.display = "";
+        
+      });
+
+
+    }else{
+
+
+
+      let rows = document.getElementsByTagName('tr');
+
+      Array.from(rows).forEach((row, index)=>{
+        if(index==0){
+            return;
+        }
+        let category = row.getElementsByTagName('td')[2].innerText;
+        if(category == selectElem.value){
+        row.style.display = "";
+    }else{
+            row.style.display = "none";
+          }
+        
+      });
+    }
+
+  }
+
+  function save() {
+    let stringified = JSON.stringify(taskList);
+    localStorage.setItem('taskList', stringified);
 }
+    function load(){
+        let retrieved = localStorage.getItem('taskList');
+        taskList = JSON.parse(retrieved);
+        console.log(typeof taskList)
+        if (taskList == null)
+        taskList = [];
+    }
 
-function deleteCheck(e) {
-    const item = e.target;
-    //remove task
+    function renderRows() {
+        taskList.forEach(task => {
+            renderRow(task, null);
+        })
+    }
 
-    if (item.classList[0] === 'trash-btn') {
-        const task = item.parentElement;
-        taskButton.remove()
+    function renderRow({task: inputValue, category: inputValue2, date, time, done}) {
+
+        let table = document.getElementById('taskTable');
+
+        let trElem = document.createElement('tr');
+        table.appendChild(trElem);
+    
+        //checkbox
+    
+        let checkboxElem = document.createElement('input');
+        checkboxElem.type = "checkbox";
+        checkboxElem.addEventListener('click', done, false)
+        let tdElem1 = document.createElement('td');
+        tdElem1.appendChild(checkboxElem);
+        trElem.appendChild(tdElem1);
+
+        //date
+        
+        let dateElem = document.createElement('td');
+        dateElem.innerText = date;
+        trElem.appendChild(dateElem);
+
+
+        //time
+        
+        let timeElem = document.createElement('td');
+        timeElem.innerText = time;
+        trElem.appendChild(timeElem);
+
+    
+        //task
+    
+        let tdElem2 = document.createElement('td');
+        tdElem2.innerText = inputValue;
+        trElem.appendChild(tdElem2);
+    
+        //category
+    
+        let tdElem3 = document.createElement('td');
+        tdElem3.innerText = inputValue2;
+        trElem.appendChild(tdElem3);
+    
+        //delete 
+    
+         let spanElem = document.createElement("span");
+        spanElem.innerText = "delete";
+        spanElem.className = "material-icons";
+        spanElem.addEventListener('click', deleteItem, false);
+        let tdElem4 = document.createElement('td');
+        tdElem4.appendChild(spanElem);
+        trElem.appendChild(tdElem4);
+
+        
+    function deleteItem(){
+        trElem.remove();
+      }
+  
+    function done(){
+        trElem.classList.toggle('strike');
+        
+      }
     }
 }
